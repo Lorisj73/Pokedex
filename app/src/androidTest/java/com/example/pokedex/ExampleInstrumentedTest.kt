61ad4b91e6500178299c10adafddd7bf14324980
Lorisj73 <lorisj73@gmail.com>
2024-01-20 16:37:59 +0100

    Second commit

--- app/src/androidTest/java/com/example/pokedex/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/pokedex/ExampleInstrumentedTest.kt
@@ -15,11 +15,7 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+// J'ai essayé de faire une série de tests mais je n'arrive pas à les faire marcher
 @RunWith(AndroidJUnit4::class)
 class HomeScreenInstrumentedTest {
 
--- app/src/main/java/com/example/pokedex/data/AppContainer.kt
+++ app/src/main/java/com/example/pokedex/data/AppContainer.kt
@@ -8,39 +8,24 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 
-/**
- * Dependency Injection container at the application level.
- */
+
 interface AppContainer {
     val pokemonRepository: PokemonRepository
 }
 
-/**
- * Implementation for the Dependency Injection container at the application level.
- *
- * Variables are initialized lazily and the same instance is shared across the whole app.
- */
+
 class DefaultAppContainer : AppContainer {
     private val baseUrl = "https://raw.githubusercontent.com/Josstoh/res508-qualite-dev-android/main/rest/"
 
-    /**
-     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
-     */
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
 
-    /**
-     * Retrofit service object for creating api calls
-     */
     private val retrofitService: PokemonApiService by lazy {
         retrofit.create(PokemonApiService::class.java)
     }
 
-    /**
-     * DI implementation for Pokemon repository
-     */
     override val pokemonRepository: PokemonRepository by lazy {
         NetworkPokemonRepository(retrofitService)
     }
